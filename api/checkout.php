7d22f8eb620ea8bab36e7dacc135861f0b411701
Akshar <aksharmangukiya8055@gmail.com>
2025-03-05 20:34:22 -0500

    created checkout api

---
+++ api/checkout.php
@@ -0,0 +1,20 @@
+<?php
+header("Content-Type: application/json");
+require_once '../model/Checkout.php';
+
+$checkoutModel = new Checkout();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    if (!isset($_POST['user_id'], $_POST['cart'], $_POST['total_price'], $_POST['address'], $_POST['payment_method'])) {
+        echo json_encode(['success' => false, 'message' => 'Missing required fields.']);
+        exit();
+    }
+
+    $response = $checkoutModel->createOrder($_POST['user_id'], $_POST['cart'], $_POST['total_price'], $_POST['address'], $_POST['payment_method']);
+    echo json_encode($response);
+    exit();
+}
+
+echo json_encode(['success' => false, 'message' => 'Invalid request method.']);
+?>
